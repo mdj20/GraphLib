d79f52ecf920af9e03c19bfcaeefd535a582a909
mdj20 <mdj20@pitt.edu>
2017-09-19 19:05:41 -0400

    Pathfinder working

--- GraphLib/src/algorithms/PathFinder.java
+++ GraphLib/src/algorithms/PathFinder.java
@@ -105,60 +105,16 @@ public class PathFinder {
 		return path;
 	}
 	
-	/*
-	public static  <V,W extends Number & Comparable<W>> List<WeightedDirectionalEdge<V,W>> lazyDikstrasDouble(WeightedAdjacencyListDiGraph<V,W> graph, V source, V sink){
-		ArrayList<WeightedEdge<V,W>> path = new ArrayList<>();  // return path;
-		PriorityQueue<VertexInt<V,Double,W>> queue = new PriorityQueue<VertexInt<V,Double,W>>();  // priority queue used to determine the lowest tentative value of vertices' distance
-		HashSet<V> checked = new HashSet<V>();  // set of vertices that have been checked 
-		HashMap<V,VertexInt<V,Double,W>> map = new HashMap<V,VertexInt<V,Double,W>>();  
-		VertexInt<V,Double,W> sourceInfo = new VertexInt<V,Double,W>(source,0.0);
-		
-		map.put(source, sourceInfo);
-		queue.add(sourceInfo);
-		while(!queue.isEmpty() && !sink.equals(queue.peek().vertex)) {
-			sourceInfo = queue.poll(); //pop next vertexInfo
-			checked.add(sourceInfo.vertex);  // add vertex to checked list
-			for(WeightedDirectionalEdge<V,W> edge: graph.getOutgoingEdges(sourceInfo.vertex)) {  // for each outgoing edge
-				V nextV = edge.getOpposingVertex(sourceInfo.vertex);  // get opposing vertex
-				if(!map.containsKey(nextV)){ //if vertex hasn't been discovered
-					VertexInt<V,Double,W> next = new VertexInt<V,Double,W>(nextV,(sourceInfo.val+edge.getWeight().doubleValue()),edge); // create new record with updated weights and edge
-					map.put(nextV,next); // put in map
-					queue.add(next);	// add to queue
-				}
-				else if(!checked.contains(nextV)) {  // if vertex has been discoverd and it hasn't been checked
-					VertexInt<V,Double,W> next = map.get(nextV);  // get record from map
-					if((sourceInfo.val+edge.getWeight().intValue()) <next.val ) {  // and if new value is less than established value
-						queue.remove(next);  // remove from queue (must reinsert)
-						next.edge = edge;  // update info 
-						next.val = sourceInfo.val + edge.getWeight().doubleValue();
-						queue.add(next);  // add back to queue
-					}
-				}
-			}
-		}
-		if(!queue.isEmpty()&&sink.equals(queue.peek().vertex)) { //aggregate results
-			V current = queue.poll().vertex;
-			while (!source.equals(current)) {
-				path.add(map.get(current).edge);
-				current = path.get(path.size()-1).getOpposingVertex(current);
-			}
-			Collections.reverse(path);
-		}
-		for(WeightedEdge<V, W> edge: path) {
-			System.out.println(edge.getVertex(0)+" "+edge.getWeight()+" "+edge.getVertex(1));
-		}
-		return null;
-	}
 	
-	*/
 	public static void main(String args[]) {
 		WeightedAdjacencyListDiGraph<Character,Integer> graph = BuildWeightedDiGraph.getWeightedDiGraph();
-		/*lazyDikstras(graph,'A','F');
+		
+		lazyDikstras(graph,'A','F');
 		System.out.println();
 		lazyDikstras(graph,'C','A');
 		System.out.println();
 		lazyDikstras(graph,'G','C');
-		*/
+		
 		
 		
 		for (int i = 0 ; i <50 ; i++) {
