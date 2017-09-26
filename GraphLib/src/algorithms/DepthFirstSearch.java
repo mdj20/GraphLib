e7d16cb543cb6e46fbbca7ee37de14faf59533fc
Matthew Jeffreys <mdj20@pitt.edu>
2017-09-26 06:26:31 -0400

    Can't tell if the DFS is hanging from large upper bound or error.

--- GraphLib/src/algorithms/DepthFirstSearch.java
+++ GraphLib/src/algorithms/DepthFirstSearch.java
@@ -38,10 +38,11 @@ public class DepthFirstSearch {
 	}
 	
 	private static <V, E extends Edge<V>> boolean dfsRec(Graph<V,E> graph, Deque<V> stack, Set<V> set, V sink) {
-		boolean ret = false;
+		boolean ret = false;	
 		if(!sink.equals(stack.peek())) {
+
 			for(V v: graph.getOutgoingVertices(stack.peek())) {
-				System.out.println("V:"+v+" "+stack.size());
+
 				if(!set.contains(v)) {
 					push(v,stack,set);
 					if (dfsRec(graph,stack,set,sink)) {
@@ -55,31 +56,18 @@ public class DepthFirstSearch {
 			}
 		}
 		else {
+
 			ret =true;
 		}
 		return ret;
 	}
 	
 	public static void main(String args[]) {
-	/*	WeightedAdjacencyListDiGraph<Character,Integer> graph = FastGraphBuilder.getWeightedDiGraph();
-		for(Character c: DFS(graph,'A','F')) {
-			System.out.println(c);
-		}
-		for(int i = 0 ; i<50 ; i++) {
-			int j = i%3 ,k = i%7;
-			System.out.println(i+": "+FastGraphBuilder.tc(j)+" to "+FastGraphBuilder.tc(k));
-			graph = FastGraphBuilder.buildRandomDiGraph(7, 10, 0, 6);
-			for(Character c: DFS(graph,FastGraphBuilder.tc(j),FastGraphBuilder.tc(k))) {
-				System.out.println(c);
-			}
-			System.out.println();
-		}
-		
-		*/
+
 		List<Edge<Integer>> edgeList = UnivAvengersGraphData.getEdgeList(UnivAvengersGraphData.dsjc5001col);
 		SimpleAdjacencyListGraph<Integer> graph = new SimpleAdjacencyListGraph<Integer>();
-		
-		Integer high = null , low = null;
+		System.out.println("edgelist Size "+edgeList.size());
+		Integer high = 111 , low = 106;
 		for(Edge<Integer> e:edgeList) {
 			for(Integer i:e.getVertices()) {
 				if(!graph.getVertices().contains(i)){
@@ -89,26 +77,12 @@ public class DepthFirstSearch {
 			graph.addEdge(e.getVertex(0), e.getVertex(1));
 		}
 		ArrayList<Integer> verts = new ArrayList<Integer>(graph.getVertices());
-		Random rando = new Random(System.nanoTime());
-		ArrayList<Integer> pathSize = new ArrayList<>();
-		for(int i =0;i<200;i++) {
-			low = verts.get(rando.nextInt(verts.size()));
-			high = verts.get(rando.nextInt(verts.size()));
-			System.out.println("LOW"+low+" "+high);
-			List<Integer> path = DFS(graph,low,high);
-			for(Integer c: path) {
-				//System.out.println(c);
-			}
-			System.out.println(i+":");
-			pathSize.add(path.size());
-			System.gc();
-		}
-		long sum = 0;
-		for(Integer i:pathSize) {
-			sum+=i;
-		}
-		System.out.println("AVG Path: "+(sum/pathSize.size()));
 		
+		Random rando = new Random(System.nanoTime());
+		System.out.println("LOW"+low+" "+high);
+		//List<Integer> path = DFS(graph,low,high);
+		ArrayList<Integer> pathSize = new ArrayList<>();
+
 	}
 	
 	private static <V> void push(V value, Deque<V> stack, Set<V> set) {
