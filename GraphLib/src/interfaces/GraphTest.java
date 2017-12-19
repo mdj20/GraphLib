0f63281d9f59608a0d5173e9d8d94a35fa7c0e30
Matthew Jeffreys <mdj20@pitt.edu>
2017-12-19 05:52:20 -0500

    Adding unit tests in main

---
+++ GraphLib/src/interfaces/GraphTest.java
@@ -0,0 +1,19 @@
+package interfaces;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+
+@RunWith(Parameterized.class)
+public class GraphTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+}
