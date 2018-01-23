89696f438fb4229459404444c10984317ffe0aa0
Matthew Jeffreys <mdj20@pitt.edu>
2018-01-22 23:58:00 -0500

    Working on Path Builder

--- GraphLib/src/algorithms/PathBuilder.java
+++ GraphLib/src/algorithms/PathBuilder.java
@@ -1,5 +1,9 @@
 package algorithms;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import interfaces.Edge;
 import interfaces.Graph;
 import interfaces.WeightedGraph;
@@ -10,16 +14,45 @@ import interfaces.WeightedGraph;
 
 public class PathBuilder<G extends Graph<V,E>,V,E extends Edge<V>> {
 	
+	protected List<E> edgeList;
 	protected G graph;
-	Path<V,E> path;
-
-	public PathBuilder(G graph){
-		
+	protected Path<V,E> path;
+	protected V source;
+	protected V tail;
+	
+	public PathBuilder(G graph, V source){
+		this.graph=graph;
+		this.source= source;
+		this.edgeList = new ArrayList<E>();
 	}
 	
 	
-	//public addEdge(E edge)
+	public boolean addEdge(E edge) {
+		if(edgeList.size()==0) {
+			if(edge.getVertices().contains(source)) {
+				
+			}
+		}
+	}
 	
+	//protected
+	
+	protected boolean checkEdge(E edge, V edgeSource) {
+		boolean ret = false;
+		Set<E> edges = graph.getOutgoingEdges(edgeSource);
+		if(edges.contains(edge)){
+			ret = true;
+		}
+		else {
+			for(E e:edges) {
+				if(e.isReciprical(edge)) {
+					ret = true;
+					break;
+				}
+			}
+		}
+		return ret;
+	}
 	
 	public static <G extends Graph<V,E>,V,E extends Edge<V>> PathBuilder<G,V,E> getPathBuilder(G graph){
 		return new PathBuilder<G,V,E>(graph);
